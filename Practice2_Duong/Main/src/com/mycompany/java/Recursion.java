98ec3a80d5d786c77e7c44d3cbaf4947298f529d
Duong1229 <150923626+Duong1229@users.noreply.github.com>
2024-09-04 21:29:31 +0700

    Add files via upload

---
+++ Practice2_Duong/Main/src/com/mycompany/java/Recursion.java
@@ -0,0 +1,11 @@
+package com.mycompany.java;
+
+public class Recursion {
+	long factorial( long number) {
+		 if (number <= 1) // base case
+		 return 1;
+		 else
+		 return number * factorial(number - 1);
+		}
+
+}
